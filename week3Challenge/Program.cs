628b5547fdb46d303a894cb6d7138c94d8e58545
Andrew Berkey <andrewberkey03@gmail.com>
2022-04-21 10:28:31 -0500

    base employee class Properties, ToString, and Bonus method all working

--- week3Challenge/Program.cs
+++ week3Challenge/Program.cs
@@ -7,7 +7,14 @@ namespace EmployeeBonuses
     {
         static void Main (string[] args)
         {
-                
+            //test empty Employee Constructor
+            Employee testEmployee1 = new Employee();
+            Console.WriteLine(testEmployee1);
+
+            //test 'standard' Employee Constructor 
+            Employee testEmployee2 = new Employee("Employee", "Test", 'E');
+            Console.WriteLine(testEmployee2);
+            
         }
 
     }
