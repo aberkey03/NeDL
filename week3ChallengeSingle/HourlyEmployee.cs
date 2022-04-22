ff65eca24526d0b3d02e069f4837298ea8417420
Andrew Berkey <andrewberkey03@gmail.com>
2022-04-22 09:53:23 -0500

    S working on single array

---
+++ week3ChallengeSingle/HourlyEmployee.cs
@@ -0,0 +1,45 @@
+ 
+using System;
+
+namespace EmployeeBonuses
+{
+    class HourlyEmployee : Employee
+    {
+        //HourlyEmployee properties 
+        public double HourlyRate
+        {get;set;}
+
+        //METHODS
+        //HourlyEmployee Bonus method polymorphed from Base
+        public override double BonusCalculation() 
+        {
+            return HourlyRate*80;  //2 weeks compensation 
+        }
+        //Get HourlyRate for HourlyEmployee object, polymorphed from base
+        public override double GetHourlyRate()
+        {
+            return HourlyRate;
+        }
+        //HourlyEmployee ToString polymorphed from Base
+        public override string ToString()
+        {
+            return $"Employee Name: {LastName}, {FirstName}; Employee Type: {EmployeeType}; Hourly Rate: {HourlyRate}; Bonus: {BonusCalculation()} ";
+        }
+        
+        //CONSTRUCTORS
+        //empty HourlyEmployee constructor 
+        public HourlyEmployee() :base()
+        {
+            HourlyRate = -1.00;
+        }
+
+        //'standard' HourlyEmployee constuctor
+        public HourlyEmployee (string newLastName, string newFirstName, char newEmployeeType, double newHourlyRate) :base(newLastName, newFirstName, newEmployeeType)
+        {
+            HourlyRate = newHourlyRate;
+        }
+
+        
+
+    }
+}
