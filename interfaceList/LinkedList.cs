378f25094fb9786234e2c874ac4101ef9361ec74
Andrew Berkey <andrewberkey03@gmail.com>
2022-04-27 13:04:36 -0500

    messing with a linked list

---
+++ interfaceList/LinkedList.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace InterfaceList
+{
+    /* public class LinkedList 
+    {
+        Node head;
+
+        public class Node 
+        {
+            int data;
+            Node next;
+            Node(int d)
+            {
+                data = d;
+            }
+        }
+    } */
+}
