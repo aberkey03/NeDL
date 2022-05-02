0ce6083f2467118938ac18f7493d5fec38061eb0
Andrew Berkey <andrewberkey03@gmail.com>
2022-05-02 11:30:30 -0500

    corporate working

--- tier1.1Challenge/Corporate.cs
+++ tier1.1Challenge/Corporate.cs
@@ -2,8 +2,36 @@ using System;
 
 namespace CustomerMemberships
 {
-    class Corporate
+    class Corporate : MembershipGeneric
     {
+        // PROPERTIES
+            // ONLY USES BASE CLASS PROPERTIES 
+
+        // CONTRUCTOR(S)
+        public Corporate(int newMembershipID, string newPrimaryEmailAddress, string newMembershipType, double newAnnualCost, double newPuchasesMonthly) : base (newMembershipID, newPrimaryEmailAddress, newMembershipType, newAnnualCost, newPuchasesMonthly)
+        {
+            // ONLY USES BASE CLASS PROPERTIES/CONTRUCTOR
+        }
+
+        // METHODS
+
+        // toString()
+        // no unique properties or info for Corperate memberships
+        public override string ToString()
+        {
+            return $"{base.ToString()}"; 
+        }
         
+        // ApplyCashBack() overiridden from MembershipGeneric
+        // flat cashBackPercentage 
+        // display cash back, clear monthly purchases, display account overview 
+        public override void ApplyCashBack()
+        {
+            double cashBackPercentage = .01;
+            double cashBackAmt = PuchasesMonthly * cashBackPercentage;
+            Console.WriteLine($"Cash-back reward request for Membership ID {MembershipID} in the amount of ${cashBackAmt} has been made.");
+            PuchasesMonthly = 0;
+            Console.WriteLine(ToString());
+        }
     }
 }
