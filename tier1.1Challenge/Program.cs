dbb6c425351102a1ed133403174cc8fff14558b0
Andrew Berkey <andrewberkey03@gmail.com>
2022-05-02 12:20:02 -0500

    user interface set up

--- tier1.1Challenge/Program.cs
+++ tier1.1Challenge/Program.cs
@@ -87,6 +87,130 @@ namespace CustomerMemberships
 
             Console.WriteLine("Cash back");
             corporate1.ApplyCashBack(); */
-        }
-    }
-}
+
+            // TEST DATA
+            Regular regular1 = new Regular(1000, "regular1@gmail.com", "Regular", 100.00, 300.00);
+            Executive executive1 = new Executive(2000, "executive1@gmail.com", "Executive", 200, 700);
+            Executive executive2 = new Executive(2001, "executive2@gmail.com", "Executive", 200, 2000);
+            NonProfit nonProfit1 = new NonProfit(3000, "nonProfit1@gmail.com", "NonProfit", true, 50.00, 300.00);
+            NonProfit nonProfit2 = new NonProfit(3001, "nonProfit2@gmail.com", "NonProfit", false, 50.00, 300.00);
+            Corporate corporate1 = new Corporate(4000, "corporate1@gmail.com", "Corporate", 200, 6537.37);
+
+            // CREATE LIST AND ADD TEST DATA
+            List<MembershipGeneric> membershipList = new List<MembershipGeneric>();
+            membershipList.Add(regular1);
+            membershipList.Add(executive1);
+            membershipList.Add(executive2);
+            membershipList.Add(nonProfit1);
+            membershipList.Add(nonProfit2);
+            membershipList.Add(corporate1);
+
+            // INTERFACE 
+
+            // INITIAL VARIABLES
+            bool validMainMenuChoice;
+            string mainMenuChoice;
+            do // keep program running until 'Q' is entered
+            {
+                do // keep at main menu until a valid option is entered 
+                {
+                    validMainMenuChoice = false;
+                    
+                    //display options
+                    Console.WriteLine("What do you want to do?");
+                    Console.WriteLine("C - Create a new membership and add to the membership list.");
+                    Console.WriteLine("R - Read all of the memberships in the membership list.");
+                    Console.WriteLine("U - Update an existing membership based on membership ID.");
+                    Console.WriteLine("D - Delete an existing membership based on membership ID.");
+                    Console.WriteLine("L - List all of the memberships in the list including all of the information for each account type.");
+                    Console.WriteLine("P - Perform a purchase transaction by getting a membership number from the user and a purchase amount and if the membership exists add the purchase amount to the monthly purchase total.");
+                    Console.WriteLine("T - Perform a return transaction by getting an membership number from the user and a return amount and if the membership exists, perform the return by subtracting the return amount for the monthly purchase total.");
+                    Console.WriteLine("A - Apply cash-back rewards as described above by getting a membership number from the user.");
+                    Console.WriteLine("Q - Quit the transaction processing.");
+
+                    
+                    //read user entry, and verify it is valid
+                    mainMenuChoice = Console.ReadLine();
+                    validMainMenuChoice = (mainMenuChoice == "C" || mainMenuChoice == "c" 
+                        || mainMenuChoice == "R" || mainMenuChoice == "r" 
+                        || mainMenuChoice == "U" || mainMenuChoice == "u" 
+                        || mainMenuChoice == "D" || mainMenuChoice == "d"
+                        || mainMenuChoice == "L" || mainMenuChoice == "l"
+                        || mainMenuChoice == "P" || mainMenuChoice == "p"
+                        || mainMenuChoice == "T" || mainMenuChoice == "t"
+                        || mainMenuChoice == "A" || mainMenuChoice == "a"
+                        || mainMenuChoice == "Q" || mainMenuChoice == "q");
+                    //if not valid tell to enter valid option
+                    if (!validMainMenuChoice) 
+                    {
+                        Console.WriteLine("Please enter a valid option.");
+                    }
+                } while (!validMainMenuChoice);
+                
+                /* C - Create a new membership and add to the membership list.  
+                Be sure you don't duplicate the membership ID.  It needs to be unique. */
+                if (mainMenuChoice == "C" || mainMenuChoice == "c")
+                {
+                    Console.WriteLine("In the 'C' area.");
+                }
+
+                /* DONE
+                R - Read all of the memberships in the membership list.
+                same as L */
+                if (mainMenuChoice == "R" || mainMenuChoice == "r")
+                {
+                    Console.WriteLine("In the 'R' area.");
+                    foreach (MembershipGeneric membershipDetails in membershipList)
+                    {
+                        Console.WriteLine(membershipDetails);
+                    }
+                }
+
+                /* U - Update an existing membership based on membership ID. */
+                if (mainMenuChoice == "U" || mainMenuChoice == "u")
+                {
+                    Console.WriteLine("In the 'U' area.");
+                }
+
+                /* D - Delete an existing membership based on membership ID. */
+                if (mainMenuChoice == "D" || mainMenuChoice == "d")
+                {
+                    Console.WriteLine("In the 'D' area.");
+                }
+
+                /* DONE
+                L - List all of the memberships in the list including all of the information for each account type.
+                same as R */
+                if (mainMenuChoice == "L" || mainMenuChoice == "l")
+                {
+                    Console.WriteLine("In the 'L' area.");
+                    foreach (MembershipGeneric membershipDetails in membershipList)
+                    {
+                        Console.WriteLine(membershipDetails);
+                    }    
+                }
+
+                /* P - Perform a purchase transaction by getting a membership number from the user and a purchase amount
+                and if the membership exists add the purchase amount to the monthly purchase total. */
+                if (mainMenuChoice == "P" || mainMenuChoice == "p")
+                {
+                    Console.WriteLine("In the 'P' area.");
+                }
+
+                /* T - Perform a return transaction by getting an membership number from the user and a return amount 
+                and if the membership exists, perform the return by subtracting the return amount for the monthly purchase total. */
+                if (mainMenuChoice == "T" || mainMenuChoice == "t")
+                {
+                    Console.WriteLine("In the 'T' area.");
+                }
+
+                /* A - Apply cash-back rewards as described above by getting a membership number from the user. */
+                if (mainMenuChoice == "A" || mainMenuChoice == "a")
+                {
+                    Console.WriteLine("In the 'A' area.");
+                }
+
+            }while (!(mainMenuChoice == "Q" || mainMenuChoice == "q")); //end main loop
+        } //end Main emthod
+    }  //end Program class
+}  // end NameSpace
