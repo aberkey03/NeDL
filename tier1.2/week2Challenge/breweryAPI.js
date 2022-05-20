79579cea001d2bbe8d31ee912a11e6e0216354ea
Andrew Berkey <andrewberkey03@gmail.com>
2022-05-20 14:37:07 -0500

    first comit of the day!

--- tier1.2/week2Challenge/breweryAPI.js
+++ tier1.2/week2Challenge/breweryAPI.js
@@ -7,6 +7,10 @@
 } */
 
 async function getBreweriesSearch(){
+    //clear previous resuls
+    let resultsDiv = document.getElementById("results_div").innerHTML;
+    resultsDiv = '';
+
     //declare base api URL
     let apiString = "https://api.openbrewerydb.org/breweries"; //https://www.openbrewerydb.org/documentation
     console.log(apiString);
@@ -15,15 +19,21 @@ async function getBreweriesSearch(){
     let newLocationTypeInput = document.getElementById("search_location_type").value; //city, state, or zip
     let newLocationStringInput = document.getElementById("search_location_text").value; //the city, state, or zip to search
 
-    // TODO replace spaces with underscore "_"
+    //replace spaces with _
+    newLocationStringInput = newLocationStringInput.replace(/\s/g, '_');
+    console.log(newLocationStringInput);
     
-    if (newLocationTypeInput != ""){
+    if (newLocationTypeInput != ''){
         apiString = apiString + "?" + newLocationTypeInput + "=" + newLocationStringInput;
     }
     else{
         apiString += "?";
     };
     console.log(apiString);
+
+    //add brewery types
+    let breweryType = document.getElementsByName("brewery_type").innerHTML;
+    console.log(breweryType);
 
     //add sort order 
     let newSortOrder = document.getElementById("sort_order_value").innerHTML; //ascending or decending (name by default)
@@ -51,7 +61,6 @@ async function getBreweriesSearch(){
 
     //clear old text
     let resultsBody = document.getElementById("results_anchor").innerHTML;
-    resultsBody = "";
 
     //create JSON object
     const jsonText = await response.json();
@@ -63,19 +72,47 @@ async function getBreweriesSearch(){
         let breweryType = jsonText[i].brewery_type;
         let breweryCity = jsonText[i].city;
         let breweryState = jsonText[i].state;
-        let breweryCountyProvince = jsonText[i].county_province;
+        let breweryCountry = jsonText[i].country;
         let breweryWebsite = jsonText[i].website_url;
         //create new paragraph for each result and then add 
-        let outputParagraph = "<p class=\"" + jsonText[i].brewery_type + "\" > Brewery Name: " + breweryName + "<br> Type: " + breweryType + "<br> City: " + breweryCity + "<br> State: " + breweryState + "<br> County/Province: " + breweryCountyProvince + "<br> Website: " + "<a href=\"" + breweryWebsite + "\">"  + breweryWebsite + "</a>" + "</p>";
+        let outputParagraph = "<p class= \" " + jsonText[i].brewery_type + "\" > Brewery Name: " + breweryName + "<br> Type: " + breweryType + "<br> City: " + breweryCity + "<br> State: " + breweryState + "<br> Country: " + breweryCountry + "<br> Website: " + "<a href=\"" + breweryWebsite + "\">"  + breweryWebsite + "</a>" + "</p>";
         document.getElementById("results_anchor").innerHTML += outputParagraph;
     };
 }
 
 async function getRandomBrewery(){
+    //clear previous spotlight
+    let resultsBody = document.getElementById("spotlight_anchor").innerHTML;
+    resultsBody = "";
+    console.log("Spotlight cleared!");
+    
+    //declare base api URL
+    let apiString = "https://api.openbrewerydb.org/breweries/random"; //https://www.openbrewerydb.org/documentation
+    console.log(apiString);
 
+    //call API
+    let response = await fetch(apiString, {cache: "no-cache"});
+
+    //create JSON object
+    const jsonText = await response.json();
+
+    //declare variables for the brewery details
+    
+    let breweryName = jsonText[0].name;
+    let breweryType = jsonText[0].brewery_type;
+    let breweryCity = jsonText[0].city;
+    let breweryState = jsonText[0].state;
+    let breweryCountry = jsonText[0].country;
+    let breweryWebsite = jsonText[0].website_url;
+    //create new paragraph for each result and then add 
+    let outputParagraph = "<p class= \" " + jsonText[0].brewery_type + "\" > Brewery Name: " + breweryName + "<br> Type: " + breweryType + "<br> City: " + breweryCity + "<br> State: " + breweryState + "<br> Country: " + breweryCountry + "<br> Website: " + "<a href=\"" + breweryWebsite + "\">"  + breweryWebsite + "</a>" + "</p>";
+    document.getElementById("spotlight_anchor").innerHTML = outputParagraph;
+    console.log("P: " + outputParagraph);
+    console.log("APIstring: " + apiString);
 }
 
-function updateNumberPerPageDisplay(){
+function clear(){
+    document.getElementById("'results_div'").innerHTML = "";
 
 }
 
