a5d3e3e41aa5d14440f73924b782c400f8fba5b1
Andrew Berkey <andrewberkey03@gmail.com>
2022-05-25 10:07:54 -0500

    minor updates

--- Java/collectionMVC/Main.java
+++ Java/collectionMVC/Main.java
@@ -2,29 +2,29 @@ public class Main {
     public static void main(String[] args) {
 
         //default model constructor, should start with default values
-        AccountModel testAccount1 = new AccountModel();
+        AccountModel testAccount = new AccountModel();
 
         //view (no contructor), will use to display the values of the account properties
         AccountView accountView = new AccountView();
 
         //controller to interact with the account model and view
-        AccountControler accountControler1 = new AccountControler(testAccount1, accountView);
+        AccountControler accountControler = new AccountControler(testAccount, accountView);
 
         //print default values
-        accountControler1.updateAccountView();
+        accountControler.updateAccountView();
 
         //use controler to update values
-        accountControler1.setAccountType("Checking");
-        accountControler1.setAccountID(111);
-        accountControler1.setAccountBalance(1000.0);
+        accountControler.setAccountType("Checking");
+        accountControler.setAccountID(111);
+        accountControler.setAccountBalance(1000.0);
 
         //print updated values
-        accountControler1.updateAccountView();
+        accountControler.updateAccountView();
 
-        //make a deposit using the controller
-        accountControler1.makeTransaction();
+        //make a transaction (deposit or withdraw) using the controller
+        accountControler.makeTransaction();
 
         //print updated values
-        accountControler1.updateAccountView();
+        accountControler.updateAccountView();
     }
   }
